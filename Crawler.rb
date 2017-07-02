edf6ddfabaeeb1d4f1c1088ecbaba289d7349cba
Davi Campos Gonçalves <davi19_19@hotmail.com>
2017-07-02 14:33:10 -0300

    Feito codigo

--- Crawler.rb
+++ Crawler.rb
@@ -1,1 +1,26 @@
-navegador = Watir::Browser.start ""
+require "nokogiri"
+require 'watir'
+
+navegador = Watir::Browser.start "https://www.ncbi.nlm.nih.gov/pubmed/?term=diabetes"
+site = open("https://www.ncbi.nlm.nih.gov/pubmed/?term=diabetes")
+documento =Nokogiri::HTML(site)
+
+pagina=0
+while(pagina!=20)
+  pmid=documento.css(".rprtid/dd")
+  pmid[0..-1].each do |row|
+    paginaArtigo=open("https://www.ncbi.nlm.nih.gov/pubmed/#{row.text}")
+    documentoArtigo= Nokogiri::HTMl(paginaArtigo)
+    autores =documentoArtigo.css(".auths")
+    filiacao =documentoArtigo.css(".afflist")
+    resumo =documentoArtigo.css(".abstr")
+    palavrasChave =documentoArtigo.css(".keywords")
+    if(resumo.empty?|| autores.empty?)
+      puts "Vazio"
+    else
+      artigo = autores.text+"\n"+filiacao+"\n"+resumo+"\n"+palavrasChave
+      puts artigo
+    end
+  end
+  pagina=pagina+1
+end
